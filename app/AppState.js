f914931d17e528626337fef1537044742a9c0bc3
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-05 12:30:31 -0700

    Added player objects into appstate array

--- app/AppState.js
+++ app/AppState.js
@@ -1,3 +1,4 @@
+import { PingPongPlayer } from './models/PingPongPlayer.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
@@ -8,7 +9,11 @@ class ObservableAppState extends EventEmitter {
 
 
   // NOTE creating place to store player data in our application. All pieces of data should be stored in the class definition itself for Observable AppState
-  pingPongPlayers = []
+  pingPongPlayers = [
+    new PingPongPlayer('Jake', 'https://images.unsplash.com/photo-1517466787929-bc90951d0974?q=80&w=2186&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+
+    new PingPongPlayer('Jeremy', 'https://images.unsplash.com/photo-1516820612845-a13894592046?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+  ]
 
 }
 
