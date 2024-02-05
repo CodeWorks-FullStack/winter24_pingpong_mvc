909df59415aa6aaed4077c14091ab91c23d33f12
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-05 14:13:18 -0700

    logging players in appstate

--- app/controllers/PingPongPlayersController.js
+++ app/controllers/PingPongPlayersController.js
@@ -1,7 +1,20 @@
+import { AppState } from "../AppState.js";
+
 
 export class PingPongPlayersController {
+  // NOTE always put constructor at top or bottom of class definition
 
+  // NOTE this will load on page load if our router has this controller set up in one of its objects
   constructor () {
     console.log('PING PONG PLAYERS CONTROLLER LOADED!');
+    this.drawPingPongPlayers()
+  }
+
+
+  // NOTE method
+  drawPingPongPlayers() {
+    console.log('DRAWING PLAYERS');
+    const players = AppState.pingPongPlayers
+    console.log('Here are the players', players);
   }
 }
